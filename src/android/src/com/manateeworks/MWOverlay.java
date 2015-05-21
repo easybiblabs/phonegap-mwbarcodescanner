97d4cdbc39d6837f59916e39073c7d41e94bf65a
Thorben Schröder <info@thorbenschroeder.de>
2015-05-21 17:45:05 +0200

    Fixes vertical position of the hint on Android
    This would appear over the viewfinder on some devices, before.

--- src/android/src/com/manateeworks/MWOverlay.java
+++ src/android/src/com/manateeworks/MWOverlay.java
@@ -15,93 +15,104 @@ import android.util.TypedValue;
 import android.graphics.Typeface;
 import android.view.Display;
 import android.view.WindowManager;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 public class MWOverlay extends View {
-	
+
   private static final String TAG = "Barcodescanner Overlay";
-  
+
 	private static boolean isAttached = false;
 
 	private static MWOverlay viewportLayer;
   private static TextView textView;
   private static TextView viewFinderView;
-  
-	public static MWOverlay addOverlay (ScannerActivity context, View previewLayer) {	
+
+	public static MWOverlay addOverlay (ScannerActivity context, View previewLayer) {
 		isAttached = true;
-    
+
     Display display = ((WindowManager) context.getSystemService(context.WINDOW_SERVICE)).getDefaultDisplay();
 
     int width = display.getWidth();
     int height = display.getHeight();
     int orientation = display.getOrientation();
-		
+
 		ViewGroup parent = (ViewGroup) previewLayer.getParent();
-    
+
 		viewportLayer = new MWOverlay(context);
-    
+
 		ViewGroup.LayoutParams rl = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
     parent.addView(viewportLayer, rl);
-    
-    textView = new TextView(context); 
+
+    textView = new TextView(context);
     textView.setVisibility(View.VISIBLE);
-    
+
     String prompt = (String) context.customParams.get("PROMPT");
     textView.setText(prompt);
 
     textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25.f);
-    textView.setGravity(Gravity.CENTER);
+    textView.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.TOP);
     textView.setTextColor(Color.parseColor("#FFFFFFFF"));
     textView.setShadowLayer(4.f, 0.f, 2.f, Color.parseColor("#80333333"));
     textView.setTranslationY(height / 4.f);
     ViewGroup.LayoutParams textLayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
     parent.addView(textView, textLayoutParameters);
-    
+
     Typeface typeface = null;
     try {
       typeface = Typeface.createFromAsset(context.getAssets(), "www/fonts/ionicons.ttf");
     } catch (RuntimeException e) {
       Log.e(TAG, "Could not find ionicons");
     }
-    
+
     if (typeface != null) {
-      viewFinderView = new TextView(context); 
+      String viewFinderLabel = "\uf346";
+      viewFinderView = new TextView(context);
       viewFinderView.setTypeface(typeface);
       viewFinderView.setVisibility(View.VISIBLE);
-      viewFinderView.setText("\uf346");
-      
+      viewFinderView.setText(viewFinderLabel);
+
       viewFinderView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, (int) ((float) width / 2.5));
       viewFinderView.setGravity(Gravity.CENTER);
       viewFinderView.setTextColor(Color.parseColor("#FFFFFFFF"));
       viewFinderView.setShadowLayer(4.f, 0.f, 2.f, Color.parseColor("#80333333"));
+
       ViewGroup.LayoutParams viewFinderLayoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
       parent.addView(viewFinderView, viewFinderLayoutParams);
+
+      Paint paint = new Paint();
+      paint.setTypeface(viewFinderView.getTypeface());
+      paint.setTextSize(viewFinderView.getTextSize());
+      Rect bounds = new Rect();
+      paint.getTextBounds(viewFinderLabel, 0, 1, bounds);
+      textView.setTranslationY((float) height / 2.f + (float) bounds.height() / 1.8f);
     }
-    
+
     viewportLayer.postInvalidate();
-		
-		return viewportLayer;		
+
+		return viewportLayer;
 	}
-	
+
 	public static void removeOverlay () {
-		
+
 		if (!isAttached)
 			return;
-		
+
 		if (viewportLayer == null) {
 			return;
     }
-		
+
 		ViewGroup viewParent = (ViewGroup) viewportLayer.getParent();
-		
+
 		if (viewParent != null) {
 			viewParent.removeView(viewportLayer);
       viewParent.removeView(textView);
       viewParent.removeView(viewFinderView);
 		}
 	}
-	
+
 	public MWOverlay(Context context) {
 		super(context);
 	}
-  
+
 }
